8b8fbc87ab52c6ccb2668fbaf55c9acfeef8a236
Yasinebr <ebrahimiy07@gmail.com>
2025-05-14 13:32:11 +0330

    'changenew'

--- face_recognition_project/settings.py
+++ face_recognition_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-change-this-to-a-real-secret-key-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '185.204.169.218', 'abrvira.ir', 'www.abrvira.ir']
 
 # Application definition
 INSTALLED_APPS = [
